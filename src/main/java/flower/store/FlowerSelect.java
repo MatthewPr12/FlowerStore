afdf3058c04468c7602631b3b62a88ead9bd1739
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2022-10-22 12:41:09 +0300

    declaration order

--- src/main/java/flower/store/FlowerSelect.java
+++ src/main/java/flower/store/FlowerSelect.java
@@ -7,12 +7,15 @@ public class FlowerSelect {
     private static final int NUM_OF_FLOWER_TYPES = 3;
 
     public static Flower selectFlower(int channel) {
-        return switch (channel) {
-            case 0 -> new Rose();
-            case 1 -> new Chamomile();
-            case 2 -> new Tulip();
-            default -> throw new
-                    IllegalArgumentException("Flower selection failure");
-        };
+        switch (channel) {
+            case 0:
+                return new Rose();
+            case 1:
+                return new Chamomile();
+            case 2:
+                return new Tulip();
+            default:
+                throw new IllegalArgumentException("Wrong picked flower");
+        }
     }
 }
